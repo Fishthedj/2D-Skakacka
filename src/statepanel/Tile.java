dfaf9895e62f197929b1b5d9c53e0eb529f72490
Pavel Zvolanek <pavel.zvolanek@gmail.com>
2014-11-11 22:28:10 +0100

    Added class Tile used for storing information about individual tiles.

---
+++ src/statepanel/Tile.java
@@ -0,0 +1,16 @@
+package statepanel;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    private final TileType type;
+    private final BufferedImage image;
+    
+    public Tile(TileType type, BufferedImage image){
+        this.type = type;
+        this.image = image;
+    }
+    public enum TileType{
+        NONBLOCKING, BLOCKING;
+    }    
+}
